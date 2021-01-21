42c2fe23158d0ad123aba21517116f705ff547c4
VladLopes <57927389+Vlad-Lopes@users.noreply.github.com>
2021-01-21 13:30:40 -0300

    A Hello Word app
    Another simple Hello Word app

---
+++ JustForTest2/JustForTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustForTest2
+//
+//  Created by Vlad Lopes on 07/05/20.
+//  Copyright © 2020 Vlad Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
