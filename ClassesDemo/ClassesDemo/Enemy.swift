8b55d4e419ba5cc38cd810c8d1ef039bef03114b
VladLopes <57927389+Vlad-Lopes@users.noreply.github.com>
2021-01-21 13:11:07 -0300

    Just a simple demo
    Just a simple demo use of classes

---
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,16 @@
+
+
+class Enemy {
+    var healthy = 100
+    var attackStrenght = 10
+    
+    func move() {
+        print("Walk forwards.")
+    }
+    
+    func attack() {
+        print("Land a hit, does \(attackStrenght) damage.")
+    }
+}
+
+
---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,8 @@
+let dragon = Dragon()
+
+dragon.move()
+dragon.attack()
+
+dragon.move()
+dragon.talk(speech: "My teeth are swrords")
+
