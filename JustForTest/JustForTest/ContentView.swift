154da170e677f29dc5eb5bc23457e60c67c90ce5
VladLopes <57927389+Vlad-Lopes@users.noreply.github.com>
2021-01-21 13:29:48 -0300

    Hello Word app
    Simple Hello Word app

---
+++ JustForTest/JustForTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustForTest
+//
+//  Created by Vlad Lopes on 07/05/20.
+//  Copyright © 2020 Vlad Lopes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
