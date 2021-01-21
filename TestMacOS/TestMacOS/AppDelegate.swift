3e2d8aa93cf687d59e33976b530ba0648714666a
VladLopes <57927389+Vlad-Lopes@users.noreply.github.com>
2021-01-21 13:41:23 -0300

    Hello Word in MacOS
    Simple MacOs app.

---
+++ TestMacOS/TestMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestMacOS
+//
+//  Created by Vlad Lopes on 01/06/20.
+//  Copyright © 2020 Vlad Lopes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestMacOS/TestMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestMacOS
+//
+//  Created by Vlad Lopes on 01/06/20.
+//  Copyright © 2020 Vlad Lopes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
